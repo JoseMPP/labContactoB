8d79ea4773a84e6719740e20df70086c534769a1
user <josmapizarro.99@gmail.com>
2019-08-23 20:36:47 -0400

    Commit 2

---
+++ Contacto/Contacto/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacto.Models
+{
+    public enum TypeContact
+    {
+        Telephone,
+        Email,
+        Facebook
+    }
+    public class Contact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        [Display(Description ="Contact")]
+        public string ContactValue { get; set; }
+    }
+}
